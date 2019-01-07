f6e926494c4ee1e1569445a81ca40a9987d2e609
assignon <kevineasky@gmail.com>
2019-01-07 14:52:24 +0100

    allauth toegevoegd, facebook login gemaakt, twitter login half klaar

---
+++ co2ok_dojo/users/tests/test_fb.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+
+def set_up_allauth():
+    site = Site.objects.create(name="localhost", domain="localhost")
+    fb = SocialApp.objects.create(
+        provider="facebook",
+        name="Facebook",
+        client_id="<id>",
+        secret="<secret>",
+    )
+    fb.sites.add(site)
+    fb.save()
+
+class CollectionPageTest(TestCase):
+    def setUp(self):
+        set_up_allauth()
+
+    def test_collection_visitor(self):
+        # import pdb; pdb.set_trace()
+        response = self.client.get(reverse("collection"))
+        self.assertRedirects(
+            response, reverse("account_login"), status_code=302
+        )
