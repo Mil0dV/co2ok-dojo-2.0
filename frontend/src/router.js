e0780f27020bfff713bf7f4d73c0c5836e108d05
Shaif821 <shaif.bhaggoe@gmail.com>
2019-04-24 11:23:03 +0200

    route

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from './store'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -23,4 +24,19 @@ export default new Router({
       component: () => import( './views/Dashboard.vue')
     }
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  const publicPages = ['/login', '/register']
+  const authRequired = !publicPages.includes(to.path)
+  const loggedIn = store.state.userStatus
+
+  if(authRequired && !loggedIn) {
+    return next('/login')
+  }
+
+  next();
+
+});
+
+export default router;
