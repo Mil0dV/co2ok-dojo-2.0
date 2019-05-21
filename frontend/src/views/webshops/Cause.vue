85d1b1aca5d09fbea6b060f4177093255bb09cf9
Shaif821 <shaif.bhaggoe@gmail.com>
2019-05-21 10:09:20 +0200

    Retailer pagina afgerond + begin gemaakt aan cause pagina

---
+++ frontend/src/views/webshops/Cause.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <Header :image="header"></Header>
+
+
+    </div>
+</template>
+
+<script>
+    const Header = () => import('@/components/layout/Header')
+
+    export default {
+        name: "About",
+        components: {'Header': Header},
+
+        data() {
+            return {
+                header: 'cause-marketing',
+            }
+        },
+    }
+</script>
+
+<style scoped lang="scss">
+    .main /deep/ {
+        @import "../../../node_modules/uikit/dist/css/uikit.min.css";
+    }
+
+    @import '../../styles/main';
+    @import '../../styles/cause';
+
+</style>
