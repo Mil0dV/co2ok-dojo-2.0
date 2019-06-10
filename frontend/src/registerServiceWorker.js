081be0dfd42dd2dc637e8689760e3746fb38298a
Shaif821 <shaif.bhaggoe@gmail.com>
2019-06-10 22:20:52 +0200

    added pwa

--- frontend/src/registerServiceWorker.js
+++ frontend/src/registerServiceWorker.js
@@ -5,7 +5,13 @@ import { register } from 'register-service-worker'
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
-      console.log('Site is ready')
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB'
+      )
+    },
+    registered () {
+      console.log('Service worker has been registered.')
     },
     cached () {
       console.log('Content has been cached for offline use.')
@@ -14,16 +20,13 @@ if (process.env.NODE_ENV === 'production') {
       console.log('New content is downloading.')
     },
     updated () {
-      console.log('New content is available; Refresh...')
-      setTimeout(() => {
-        window.location.reload(true)
-      }, 10)
+      console.log('New content is available; please refresh.')
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
     },
     error (error) {
       console.error('Error during service worker registration:', error)
-    },
+    }
   })
 }
