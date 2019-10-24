dc3b76cad5ba01ec4a3d1ec13b8f29fd9e8100a7
Milo de Vries <intensmilo@gmail.com>
2019-10-24 15:35:35 +0200

    Fixes logo and widgetmark

--- frontend/public/widget/co2okWidgetMark.js
+++ frontend/public/widget/co2okWidgetMark.js
@@ -17,6 +17,12 @@ let Co2okWidget = {
   },
 
   merchantCompensations: function (widgetContainer, merchantId) {
+
+      if (document.cookie.match(/^(.*;)?\s*co2ok_hide_button\s*=\s*[^;]+(.*)?$/)){
+        console.log('hammer time!')
+        return
+      }
+
       let xhr = Co2okWidget.xhr()
       // let host = 'http://127.0.0.1:8000'
       let host = 'https://app.co2ok.eco'
@@ -57,78 +63,19 @@ let Co2okWidget = {
       fileref.setAttribute("href", `${SITE_HOST}/widget/co2okWidgetMark.css`)
       document.getElementsByTagName("head")[0].appendChild(fileref)
 
-      var image_url = plugin.url;
 
-        let widgets = `
-
-        <div class="widgets" style="width: 100%;height: auto;display: flex;flex-direction: column;justify-content: center;align-items: center;">
-              <div style="display:flex;flex-direction:column;justify-content:flex-start;align-items:flex-end;width:100%;height:auto;margin-left: 15px;">
-                  <div style="display:flex;flex-direction:row;justify-content:flex-start;align-items:center;width:380px;height:auto">
-                      <span style="font-family: Segoe UI,Frutiger,Frutiger Linotype,Dejavu Sans,Helvetica Neue,Arial,sans-serif; font-size: 12px;margin-left: 20px;">SHOP </span>
-                      <img src="${image_url}/logo.svg" alt="" class="compensate-icon" style = "width: 46px; margin-left: 3px; margin-top: 4px;">
-
-                      
-                      
-                      <div class="co2-widget_info" style ="width: 100%;height: auto;display: flex;flex-direction: row;justify-content: flex-start;align-items: flex-start; margin-left: 15px">
-                      <span style ="font-family: Segoe UI,Frutiger,Frutiger Linotype,Dejavu Sans,Helvetica Neue,Arial,sans-serif; text-align: right;font-size: 16px; font-weight: bold; margin-top: 15px;"> ${totalCompensatedData} </span>
-                      
-                      <div style="display:flex;flex-direction:column;justify-content:flex-start;align-items:flex-start;width:100%;height:auto;margin-left: 5px;">
-                      <div style="display:flex;flex-direction:row;justify-content:flex-start;align-items:center;width:100%;height:auto;margin-top: -5px;">
-                      <!-- <img src="/static/cloud.png" alt="" class="compensate-icon" style = "width: 16px;height: 16px;"> -->
-                      <p style="font-family: Segoe UI,Frutiger,Frutiger Linotype,Dejavu Sans,Helvetica Neue,Arial,sans-serif; font-size: 12px;font-weight:400;margin-top: 25px;">kg CO₂-reductie</p>
-                      <span class="co2ok_info_hitarea">
-                          <img src="${SITE_HOST}/static/info-MKS.svg" alt="" class="co2ok_widget_info" style = "width: 17px;height: 17px; padding: 2px;">
-                          </span>
-                          </div>
-                          </div>
-                   
-                  <!--
-                  <p style="color: #464646; font-family: Segoe UI,Frutiger,Frutiger Linotype,Dejavu Sans,Helvetica Neue,Arial,sans-serif; font-size: 10px;margin-top: -5px;text-align:left;">kilo CO₂ voorkomen</p>
-                  -->
-                  </div>
-                  </div>
-                  
-                  </div>
-                  </div>
-                  
-              </div>
-              <div class="co2ok_widget_infobox_container co2ok-popper" id="widget-infobox-view">
-
-                  <div class="inner-wrapper">
-                  <a href="#!" input type="text" role="button" tabindex="0" class="selectable-text first-text-to-select" style="outline: none; -webkit-appearance: none;">
-                  <p class="text-block greyBorder">Tijdens productie en vervoer van producten komen broeikasgassen vrij</p>
-                  </a>
-                  <img alt="Maak mijn aankoop klimaatneutraal " title="Maak mijn aankoop klimaatneutraal " src="${image_url}/even.svg" class="widget-svg-img-large  co2ok_info_hover_image">
-                  </div>
-                  
-                  <div class="inner-wrapper">
-                  <img alt="Maak mijn aankoop klimaatneutraal " title="Maak mijn aankoop klimaatneutraal " src="${image_url}/fout.svg" class="widget-svg-img-large co2ok_info_hover_image">
-                  <a href="#!" input type="text" role="button" tabindex="0" class="selectable-text" style="outline: none; -webkit-appearance: none;">
-                  <p class="text-block greyBorder">Met een kleine bijdrage voorkomt CO2ok evenveel uitstoot</p>
-                  </a>
-                  </div>
-          
-                  <div class="inner-wrapper">
-                  <a href="#!" input type="text" role="button" tabindex="0" class="selectable-text" style="outline: none; -webkit-appearance: none;">
-                  <p class="text-block">Zo is je aankoop klimaatneutraal!!</p>
-                  </a>
-                  </div>
-          
-                  <a class="widget-hover-link" target="_blank" href="http://co2ok.eco"><img alt="Maak mijn aankoop klimaatneutraal " title="Maak mijn aankoop klimaatneutraal " src="${image_url}/logo.svg" class="co2ok_logo_default_info widget-hover-link co2ok_logo_default_info"></a>
-                  <span class="widget-hover-link">
-                  <a  class="widget-hover-link" target="_blank" href="http://www.co2ok.eco/co2-compensatie">Hoe CO&#8322; compensatie werkt</a> </span>
-              </div>
-      
-              <div class="co2ok_infobox_container co2ok-popper" id="infobox-view">    </div>
-
-
-              `
+      try{
+        var image_url = plugin.url;
+      }
+      catch{
+        var image_url = "/wp-content/plugins/co2ok-for-woocommerce/images";
+      }
 
               let widgetimg = `<img src = "${SITE_HOST}/widget/widgetmark-grayscale.png" width=101px>`
               let widgetmark = `
               <div>
               <div class="btn_co2ok_widget co2ok_widget_info" href="#">
-                  <span class="btn_co2ok_widget co2ok_widget_info">SHOP<img class="logo_co2ok_widget" src="https://co2ok.eco/static/logo.png"></span>
+                  <span class="btn_co2ok_widget co2ok_widget_info">SHOP<img class="logo_co2ok_widget" src="${SITE_HOST}/static/logo.png"></span>
               </div>
                   <div class="caption_co2ok_widget co2ok_widget_info">
                       <span> <strong>${totalCompensatedData}</strong>t CO₂ reductie </span>
@@ -137,23 +84,23 @@ let Co2okWidget = {
                       
               <div class="co2ok_widget_infobox_container co2ok-popper" id="widget-infobox-view">
 
-              <div class="inner-wrapper">
+              <div class="widget-inner-wrapper">
               <a href="#!" input type="text" role="button" tabindex="0" class="selectable-text first-text-to-select" style="outline: none; -webkit-appearance: none;">
-              <p class="text-block greyBorder">Tijdens productie en vervoer van producten komen broeikasgassen vrij</p>
+              <p class="widget-text-block greyBorder">Tijdens productie en vervoer van producten komen broeikasgassen vrij</p>
               </a>
               <img alt="Maak mijn aankoop klimaatneutraal " title="Maak mijn aankoop klimaatneutraal " src="${image_url}/fout.svg" class="widget-svg-img-large  co2ok_info_hover_image">
               </div>
               
-              <div class="inner-wrapper">
+              <div class="widget-inner-wrapper">
               <img alt="Maak mijn aankoop klimaatneutraal " title="Maak mijn aankoop klimaatneutraal " src="${image_url}/even.svg" class="widget-svg-img-large co2ok_info_hover_image">
               <a href="#!" input type="text" role="button" tabindex="0" class="selectable-text" style="outline: none; -webkit-appearance: none;">
-              <p class="text-block greyBorder">Met een kleine bijdrage voorkomt CO2ok evenveel uitstoot</p>
+              <p class="widget-text-block greyBorder">Met een kleine bijdrage voorkomt CO₂ok evenveel uitstoot</p>
               </a>
               </div>
       
-              <div class="inner-wrapper">
+              <div class="widget-inner-wrapper">
               <a href="#!" input type="text" role="button" tabindex="0" class="selectable-text" style="outline: none; -webkit-appearance: none;">
-              <p class="text-block">Zo is je aankoop klimaatneutraal!!</p>
+              <p class="widget-text-block">Zo is je aankoop klimaatneutraal!!</p>
               </a>
               </div>
       
@@ -163,6 +110,7 @@ let Co2okWidget = {
               </div>
       
               <div class="co2ok_infobox_container co2ok-popper" id="infobox-view">    </div>
+              
               `
               // console.log(widgetimg)
 
@@ -198,18 +146,21 @@ let Co2okWidget = {
       jQuery("body").append(widgetInfoBox);
 
       if (jQuery(window).width() < 480) {
-      offset.top = offset.top + widgetInfoButton.height();
+      // offset.left = offset.left - widgetInfoBox.width() / 4;
+      offset.top = offset.top - (widgetInfoBox.height() + widgetInfoButton.height() - 9);
+      if ( offset.left < 0) offset.left = 10;
       widgetInfoBox.css({
           top: offset.top,
           margin: "0 auto",
-          left: "50%",
-          transform: "translateX(-50%)"
+          left: offset.left,
+          transform: "none"
       });
       } else {
       offset.left = offset.left - widgetInfoBox.width() / 2;
+      if ( offset.left < 0) offset.left = 10;
       offset.top = offset.top + widgetInfoButton.height();
       widgetInfoBox.css({
-          top: offset.top,
+          top: offset.top -300,
           left: offset.left,
           margin: "0",
           transform: "none"
@@ -224,10 +175,10 @@ let Co2okWidget = {
     jQuery(".co2ok_widget_container").css({
       marginBottom: 200
     });
-    if (!this.isMobile() == true ) {
-        var elmnt = document.getElementById("widget-infobox-view");
-        elmnt.scrollIntoView(false); // false leads to bottom of the infobox
-      }
+    // if (!this.isMobile() == true ) {
+    //     var elmnt = document.getElementById("widget-infobox-view");
+    //     elmnt.scrollIntoView(false); // false leads to bottom of the infobox
+    //   }
   },
 
   hideWidgetInfoBox : function()
@@ -245,9 +196,7 @@ let Co2okWidget = {
      jQuery(e.target).hasClass("widget-svg-img-large") ||
      jQuery(e.target).hasClass("logo_co2ok_widget") ||
      jQuery(e.target).hasClass("widget-text-block") ||
-     jQuery(e.target).hasClass("text-block") ||
      jQuery(e.target).hasClass("widget-inner-wrapper") ||
-     jQuery(e.target).hasClass("inner-wrapper") ||
      jQuery(e.target).hasClass("co2ok_widget_info") ||
      jQuery(e.target).hasClass("co2ok_widget_info_hitarea") ||
      jQuery(e.target).hasClass("co2ok_widget_infobox_container") ||
@@ -282,6 +231,7 @@ let Co2okWidget = {
         _this.hideWidgetInfoBox();
       }
       else {
+        _this.placeWidgetInfoBox()
         _this.ShowWidgetInfoBox();
       }
     });
@@ -298,6 +248,7 @@ let Co2okWidget = {
             _this.hideWidgetInfoBox();
           }
           else {
+            _this.placeWidgetInfoBox();
             _this.ShowWidgetInfoBox();
           }
         });
