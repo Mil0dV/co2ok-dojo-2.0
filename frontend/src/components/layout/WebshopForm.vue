984e87723101a43386a2a853e50dd9297320b134
root <root@MSI.localdomain>
2020-04-29 12:39:59 +0200

    Complete Installation Page
    - add shopworks/lightspeed buttons to forms
    - add magento installation guide
    - fix language differences
    - clean lang files

--- frontend/src/components/layout/WebshopForm.vue
+++ frontend/src/components/layout/WebshopForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="uk-section section-1" style="padding-bottom: 0px;">
+    <div class="uk-section section-1" style="padding-bottom: 120px;">
         <h1 class="main-title">{{ $t('webshopform.title2') }}</h1>
         <!-- <h1 class="main-title">Get started</h1>
         <p>Hier zou ook nog wat meer uitleg kunnen, prima hoor </p> -->
@@ -7,7 +7,7 @@
             <div class="uk-flex uk-flex-wrap uk-flex-wrap-around col-1" uk-grid>
                 <div class="uk-width-1-2 to-left">
 
-                    <div class="uk-card login__form-container col-2-card uk-card-default uk-card-body" style="box-shadow: -5px 3px 40px 2px hsla(300,3%,55%,.74);">
+                    <div class="uk-card login__form-container col-2-card uk-card-default uk-card-body" style="margin-top: 20px; height: 100%; box-shadow: -5px 3px 40px 2px hsla(300,3%,55%,.74);">
                         <form class="login__form">
                             <p style="text-align: left;">{{ $t('webshopform.subtitle2') }}</p>
                             <div class="login__group">
@@ -32,12 +32,12 @@
                                            :placeholder=" $t('webshopform.input3_field') ">
                                 </label>
 
-                                <!-- <label class="login__group">
+                                <label class="login__group">
                                     {{ $t('webshopform.input4_label') }}
                                     <input v-model="platform"
                                            class="login__group-input" type="email"
                                            :placeholder=" $t('webshopform.input4_field') ">
-                                </label> -->
+                                </label>
                             </div>
 
                             <div style="width: 100%; text-align: left;" class="login__buttons">
@@ -50,14 +50,15 @@
 
 
                 <div class="uk-width-1-2 to-right">
-                    <div class="uk-card uk-card-body section-1__col-1 final__section" style="background-color: rgb(255, 255, 255); border-radius: 5px;height: 100%;width: 100%;box-shadow: -5px 3px 40px 2px hsla(300,3%,55%,.74);">
-                        <h3 style="margin-bottom: 20px;">{{ $t('webshopform.title1') }}</h3>
+                    <div class="uk-card uk-card-body section-1__col-1 final__section" style="background-color: rgb(255, 255, 255); border-radius: 5px; margin-top: 20px; height: 100%;width: 100%;box-shadow: -5px 3px 40px 2px hsla(300,3%,55%,.74);">
+                        <h3 style="margin-top: 0px; margin-bottom: 20px;">{{ $t('webshopform.title1') }}</h3>
                         <p> {{ $t('webshopform.subtitle1') }}</p>
                         <p> <br> </p>
-                        <a href="/webshops/get-started/#magento-guide" class="" style="cursor: pointer;width: 90%;margin-bottom: 15px;"><button style="cursor: pointer;border: 0px;border-radius: 5px;color: white;background: linear-gradient(to bottom, #10dc87 0%, #08ba4d 100%);width: 100%; height: auto;padding: 15px 0px 15px 0px; border-radius: 5px;font-size:16px;text-align:center">Magento</button></a>
                         <a href="/webshops/get-started/#wc-guide" style="cursor: pointer;width: 90%;"><button style="cursor: pointer;border: 0px;background: linear-gradient(to bottom, #10dc87 0%, #08ba4d 100%);border-radius: 5px;color: white;width: 100%; height: auto;padding: 15px 0px 15px 0px; border-radius: 5px;font-size:16px;text-align:center">Wordpress / Woocommerce</button></a>
-
-                    </div>
+                        <a href="/webshops/get-started/#magento-guide" style="cursor: pointer;width: 90%;"><button style="margin-top: 15px;cursor: pointer;border: 0px;border-radius: 5px;color: white;background: linear-gradient(to bottom, #10dc87 0%, #08ba4d 100%);width: 100%; height: auto;padding: 15px 0px 15px 0px; border-radius: 5px;font-size:16px;text-align:center">Magento</button></a>
+                        <a href="/webshops/get-started/#shopify-guide" style="cursor: pointer;width: 90%;"><button style="margin-top: 15px;cursor: pointer;border: 0px;background: linear-gradient(to bottom, #10dc87 0%, #08ba4d 100%);border-radius: 5px;color: white;width: 100%; height: auto;padding: 15px 0px 15px 0px; border-radius: 5px;font-size:16px;text-align:center">Shopify</button></a>
+                        <a href="/webshops/get-started/#lightspeed_anchor" style="cursor: pointer;width: 90%;"><button style="margin-top: 15px;cursor: pointer;border: 0px;background: linear-gradient(to bottom, #10dc87 0%, #08ba4d 100%);border-radius: 5px;color: white;width: 100%; height: auto;padding: 15px 0px 15px 0px; border-radius: 5px;font-size:16px;text-align:center">lightspeed</button></a>
+                        <a href="/webshops/get-started/#shopworks_anchor" style="cursor: pointer;width: 90%;"><button style="margin-top: 15px;cursor: pointer;border: 0px;background: linear-gradient(to bottom, #10dc87 0%, #08ba4d 100%);border-radius: 5px;color: white;width: 100%; height: auto;padding: 15px 0px 15px 0px; border-radius: 5px;font-size:16px;text-align:center">Shopworks</button></a>                    </div>
                 </div>
             </div>
         </div>
--- frontend/src/views/webshops/Installation.vue
+++ frontend/src/views/webshops/Installation.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="main">
         <Header :image="header"></Header>
-        <div style="margin-top: 120px;margin-bottom: -70px;"><WebshopForm :content="formContent"/></div>
+        <div style="margin-top: 80px;margin-bottom: -70px;"><WebshopForm :content="formContent"/></div>
         <div class="uk-section" id="wc-guide">
             <div class="uk-container uk-container-width" style="">
                 <div class="install__container">
@@ -9,31 +9,31 @@
                         <h3>Wordpress / Woocommerce</h3>
 
                         <ol>
-                            <li>{{ $t('plugin.first_step1') }}</li>
+                            <li>{{ $t('plugin.WP_step0') }}</li>
 
                             <li class="list__image">
-                                {{ $t('plugin.first_step2') }}
+                                {{ $t('plugin.WP_step1') }}
                                 <img alt="plugin-wordpress-1" src="../../assets/images/webshops/plugin/install-step-1.png">
                             </li>
 
                             <li>
-                                {{ $t('plugin.first_step3') }}
+                                {{ $t('plugin.WP_step2') }}
                                 <img alt="plugin-wordpress-2" src="../../assets/images/webshops/plugin/install-step-2.png">
                             </li>
 
                             <li>
-                                {{ $t('plugin.first_step4') }}
+                                {{ $t('plugin.WP_step3') }}
                                 <img alt="plugin-wordpress-3" src="../../assets/images/webshops/plugin/install-step-3.png">
                             </li>
 
                             <li>
-                                {{ $t('plugin.first_step5') }}
+                                {{ $t('plugin.WP_step4') }}
                                 <img class="small-install" alt="plugin-wordpress-3" src="../../assets/images/webshops/plugin/install-step-4.png">
                             </li>
                         </ol>
 
                         <i>
-                            {{ $t('plugin.first_step6') }}
+                            {{ $t('plugin.WP_step5') }}
                         </i>
                     </div>
                 </div>
@@ -50,37 +50,170 @@
                         <h3>Magento</h3>
 
                         <ol>
-                            <li> {{ $t('plugin.second_step1') }}</li>
+                            <li> {{ $t('plugin.Magento_step0') }}</li>
 
                             <li class="list__image">
-                                {{ $t('plugin.second_step2') }}
-                                <img alt="plugin-wordpress-1" src="../../assets/images/webshops/plugin/install-step-1.png">
+                                {{ $t('plugin.Magento_step1') }}
+                                <img alt="plugin-wordpress-1" src="../../assets/images/webshops/plugin/magento-install1.png">
                             </li>
 
                             <li>
-                                {{ $t('plugin.second_step3') }}
-                                <img alt="plugin-wordpress-2" src="../../assets/images/webshops/plugin/install-step-2.png">
+                                {{ $t('plugin.Magento_step2') }}
+                                <img alt="plugin-wordpress-2" src="../../assets/images/webshops/plugin/magento-install3.png">
                             </li>
 
                             <li>
-                                {{ $t('plugin.second_step4') }}
-                                <img alt="plugin-wordpress-3" src="../../assets/images/webshops/plugin/install-step-3.png">
+                                {{ $t('plugin.Magento_step3') }}
                             </li>
 
                             <li>
-                                {{ $t('plugin.second_step5') }}
-                                <img class="small-install" alt="plugin-wordpress-3" src="../../assets/images/webshops/plugin/install-step-4.png">
+                                {{ $t('plugin.Magento_step4') }}
+                                <img alt="plugin-wordpress-3" src="../../assets/images/webshops/plugin/magento-install2.png">
+                            </li>
+                            <li>
+                                {{ $t('plugin.Magento_step5') }}
                             </li>
                         </ol>
-
-                        <i>
-                            {{ $t('plugin.second_step6') }}
-                        </i>
                     </div>
                 </div>
             </div>
         </div>
         <!-- <WebshopForm :content="formContent"/> -->
+
+
+        <div class="uk-section" id="shopify-guide">
+            <div class="uk-container uk-container-width" style="">
+                <div class="install__container">
+                    <div class="uk-width-1-2 install-col install-col-2">
+                        <h3>shopify</h3>
+
+                        <ol>
+                            <li v-html=" $t('plugin.Shopify_step0')"></li>
+
+                            <li class="list__image">
+                                {{ $t('plugin.Shopify_step1') }}
+                            </li>
+
+                            <li>
+                                {{ $t('plugin.Shopify_step2') }}
+                                <img alt="plugin-wordpress-2" src="../../assets/images/webshops/plugin/shopi-install-step-3.png">
+                            </li>
+
+                            <!-- <li>
+                                {{ $t('plugin.Shopify_step3') }}
+                                <img alt="plugin-wordpress-3" src="../../assets/images/webshops/plugin/install-step-3.png">
+                            </li>
+
+                            <li>
+                                {{ $t('plugin.Shopify_step4') }}
+                                <img class="small-install" alt="plugin-wordpress-3" src="../../assets/images/webshops/plugin/install-step-4.png">
+                            </li> -->
+                        </ol>
+
+                        <i>
+                            {{ $t('plugin.Shopify_step5') }}
+                        </i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <h1><a class="lightspeed_anchor" name="lightspeed_anchor"></a></h1>
+
+        <div class="uk-container form__section uk-container-width" id="Ls_form" style="margin-top: 200px;">
+            <div class="uk-flex uk-flex-wrap uk-flex-wrap-around col-1" uk-grid>
+                <div class="uk-width-1-2 to-left">
+
+                    <div class="uk-card login__form-container col-2-card uk-card-default uk-card-body" style="height: 100%; box-shadow: -5px 3px 40px 2px hsla(300,3%,55%,.74);">
+                        <form class="login__form">
+                            <p style="text-align: left;">{{ $t('webshopform.subtitle2') }}</p>
+                            <div class="login__group">
+                                <label class="login__group">
+                                    {{ $t('webshopform.input1_label') }}
+                                    <input v-model="name"
+                                           class="login__group-input" type="email"
+                                           :placeholder=" $t('webshopform.input1_field') ">
+                                </label>
+
+                                <label class="login__group">
+                                    <span>{{ $t('webshopform.input2_label') }}<span class="asterik">*</span></span>
+                                    <input v-model="email"
+                                           class="login__group-input" type="email"
+                                           :placeholder=" $t('webshopform.input2_field') ">
+                                </label>
+
+                                <label class="login__group">
+                                    {{ $t('webshopform.input3_label') }}
+                                    <input v-model="phone"
+                                           class="login__group-input" type="email"
+                                           :placeholder=" $t('webshopform.input3_field') ">
+                                </label>
+
+                                <label class="login__group">
+                                    {{ $t('webshopform.input4_label') }}
+                                    <input v-model="platform"
+                                           class="login__group-input" type="email"
+                                           :placeholder=" $t('webshopform.input4_field') ">
+                                </label>
+                            </div>
+
+                            <div style="width: 100%; text-align: left;" class="login__buttons">
+                                <a class="button btn-large" @click="sendForm()"> {{ $t('webshopform.send') }}</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <h1><a class="shopworks_anchor" name="shopworks_anchor"></a></h1>
+
+        <div class="uk-container form__section uk-container-width" style="margin-top: 200px">
+            <div class="uk-flex uk-flex-wrap uk-flex-wrap-around col-1" uk-grid>
+                <div class="uk-width-1-2 to-left">
+
+                    <div class="uk-card login__form-container col-2-card uk-card-default uk-card-body" id="shopworks_form" style="height: 100%; box-shadow: -5px 3px 40px 2px hsla(300,3%,55%,.74);">
+                        <form class="login__form">
+                            <p style="text-align: left;">{{ $t('webshopform.subtitle2') }}</p>
+                            <div class="login__group">
+                                <label class="login__group">
+                                    {{ $t('webshopform.input1_label') }}
+                                    <input v-model="name"
+                                           class="login__group-input" type="email"
+                                           :placeholder=" $t('webshopform.input1_field') ">
+                                </label>
+
+                                <label class="login__group">
+                                    <span>{{ $t('webshopform.input2_label') }}<span class="asterik">*</span></span>
+                                    <input v-model="email"
+                                           class="login__group-input" type="email"
+                                           :placeholder=" $t('webshopform.input2_field') ">
+                                </label>
+
+                                <label class="login__group">
+                                    {{ $t('webshopform.input3_label') }}
+                                    <input v-model="phone"
+                                           class="login__group-input" type="email"
+                                           :placeholder=" $t('webshopform.input3_field') ">
+                                </label>
+
+                                <label class="login__group">
+                                    {{ $t('webshopform.input4_label') }}
+                                    <input v-model="platform"
+                                           class="login__group-input" type="email"
+                                           :placeholder=" $t('webshopform.input4_field') ">
+                                </label>
+                            </div>
+
+                            <div style="text-align: left;" class="login__buttons">
+                                <a class="button btn-large" @click="sendForm()"> {{ $t('webshopform.send') }}</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
 </template>
 
