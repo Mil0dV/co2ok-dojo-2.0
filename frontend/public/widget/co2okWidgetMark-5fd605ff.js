69f65d26fe4a0df98f1224b762c13a3084881249
ghostyfied <caglois@gmail.com>
2020-05-26 13:18:49 +0200

    Update Hutspot Mark / Styling

--- frontend/public/widget/co2okWidgetMark-5fd605ff.js
+++ frontend/public/widget/co2okWidgetMark-5fd605ff.js
@@ -18,34 +18,44 @@ let Co2okWidget = {
 
   },
 
+  getCookieValue: function (a) {
+    var b = document.cookie.match('(^|[^;]+)\\s*' + a + '\\s*=\\s*([^;]+)');
+    return b ? b.pop() : '';
+  },
+
   merchantCompensations: function (widgetContainer, merchantId) {
 
-    // temp hack
-    Co2okWidget.widgetGenerator(widgetContainer, 4442)
-    return;
+    let co2ok_impact = Co2okWidget.getCookieValue('co2ok_impact')
+    
+    if (co2ok_impact > 1){
+      console.log('Collaborate and listen')
+      Co2okWidget.widgetGenerator(widgetContainer, co2ok_impact)
+      return
+    }
 
-      let xhr = Co2okWidget.xhr()
-      // let host = 'http://127.0.0.1:8000'
-      let host = 'https://app.co2ok.eco'
-      xhr.open('GET', `${host}/user/totalCompensationData/?merchantId=${merchantId}`, true)
-      //    xhr.withCredentials = true;
-         xhr.onreadystatechange = function(){
-             if (this.readyState == 4 && this.status == 200){
-              // For the near future: detect large numbers, then divide and adjust kilo to ton
-              // let totalTransactionData = (xhr.responseText / 1000).toFixed(1)
-              let totalTransactionData = xhr.responseText
-              // let totalTransactionData = 491
-
-                 console.log(totalTransactionData)
-                 Co2okWidget.widgetGenerator(widgetContainer, totalTransactionData)
-
-              // ok deze else werkt dus nog niet zoals bedoeld
-          } else {
-              let totalTransactionData = 491
-          }
+    // get impact from API
+    let xhr = Co2okWidget.xhr()
+    // let host = 'http://127.0.0.1:8000'
+    let host = 'https://app.co2ok.eco'
+    xhr.open('GET', `${host}/user/totalCompensationData/?merchantId=${merchantId}`, true)
+    xhr.onreadystatechange = function(){
+      if (this.readyState == 4 && this.status == 200){
+        // For the near future: detect large numbers, then divide and adjust kilo to ton
+        // let totalTransactionData = (xhr.responseText / 1000).toFixed(1)
+        let totalTransactionData = xhr.responseText
+        // let totalTransactionData = 491
+        
+        console.log(totalTransactionData)
+        document.cookie = 'co2ok_impact=' + totalTransactionData + ';max-age=86400;path="/"'
+        Co2okWidget.widgetGenerator(widgetContainer, totalTransactionData)
+        
+        // Something is fishy, let's serve up the total
+      } else {
+        let totalTransactionData = 491
+        Co2okWidget.widgetGenerator(widgetContainer, totalTransactionData)
       }
-      xhr.send()
-      // let totalTransactionData = 491
+    }
+    xhr.send()
       //   xhr.setRequestHeader("Authorization", `token ${window.localStorage.getItem('userToken')}`)
   },
 
@@ -59,69 +69,85 @@ let Co2okWidget = {
       let  SITE_HOST =  'https://co2ok.eco'
       // let SITE_HOST = 'http://localhost:8080'
 
+      var widgetColor = 'gray'
       var fileref=document.createElement("link")
       fileref.setAttribute("rel", "stylesheet")
       fileref.setAttribute("type", "text/css")
       fileref.setAttribute("href", `${SITE_HOST}/widget/co2okWidgetMark.css`)
       document.getElementsByTagName("head")[0].appendChild(fileref)
-      var fileref=document.createElement("link")
-      fileref.setAttribute("rel", "stylesheet")
-      fileref.setAttribute("type", "text/css")
-      fileref.setAttribute("href", `${SITE_HOST}/widget/co2okWidgetMark-5fd605ff.css`)
-      document.getElementsByTagName("head")[0].appendChild(fileref)
-
+      if (widgetColor == "gray") {
+        var colorSuffix = "-gray";
+        var fileref=document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", `${SITE_HOST}/widget/co2okWidgetMark-gray.css`)
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+     }
+     else {
+         var colorSuffix = "";
+     }
       
       if (document.cookie.match(/^(.*;)?\s*co2ok_hide_button\s*=\s*[^;]+(.*)?$/)){
         console.log('hammer time!')
         return
       }
+      // Dit moet nog ff mooier als we dit nog willen gebruiken, anders kan het weg.
+      if (totalCompensatedData < 100) {
+        var compensatiewidget  = 0.1;
+      }
+      else {
+        var compensatiewidget  = totalCompensatedData / 1000;
+      }
+      lang = 'EN'
 
-              if (totalCompensatedData <500) {
-                var compensatiewidget  = 110.5;
-                var compensatietekst = `De bij co2ok aangesloten webshops hebben samen <strong> ${compensatiewidget .toFixed(1)} </strong>ton co2-uitstoot voorkomen <br><br>= <strong>${compensatiewidget * 5000 .toFixed(0)} </strong>km vliegen`;
-              }
-              else {
-                var compensatiewidget  = totalCompensatedData / 1000;
-                var compensatietekst = `Deze webshop heeft <strong>${compensatiewidget .toFixed(1)} </strong>ton CO₂-uitstoot voorkomen <br><br>= <strong>${compensatiewidget * 5000 .toFixed(0)} </strong>km vliegen`
-              }
-
-              let widgetimg = `<img src = "${SITE_HOST}/widget/widgetmark-grayscale.png" width=101px>`
-              let widgetmark = `
-              <div>
-              <div class="btn_co2ok_widget co2ok_widget_info" href="#">
-                  <span class="btn_co2ok_widget co2ok_widget_info">SHOP<img class="logo_co2ok_widget" src="${SITE_HOST}/static/logo-gray.png"></span>
+      if (lang == 'EN') {
+        var reductietekst = 'CO₂ reduction'
+        var compensatietekst = `This webshop prevented <strong>${compensatiewidget .toFixed(1)} </strong>tonnes of CO₂ emission <br><br>= <strong>${(compensatiewidget * 5000).toFixed(0)} </strong>km of flying`
+        var how_does_it_worktekst = 'How does CO₂ offsetting work?'
+        var titletekst = 'Make my purchase Climate neutral!'
+      }
+      else {
+        var reductietekst = 'CO₂ reductie'
+        var compensatietekst = `Deze webshop heeft <strong>${compensatiewidget .toFixed(1)} </strong>ton CO₂-uitstoot voorkomen <br><br>= <strong>${(compensatiewidget * 5000).toFixed(0)} </strong>km vliegen`
+        var how_does_it_worktekst = 'Hoe werkt CO₂ compensatie?'
+        var titletekst = 'Maak mijn aankoop klimaatneutraal'
+      }
+      let widgetmark = `
+      <div>
+      <div class="btn_co2ok_widget co2ok_widget_info" href="#">
+          <span class="btn_co2ok_widget co2ok_widget_info">SHOP<img class="logo_co2ok_widget" src="${SITE_HOST}/static/logo${colorSuffix}.png"></span>
+      </div>
+          <div class="caption_co2ok_widget co2ok_widget_info">
+              <span> <strong>${(compensatiewidget.toFixed(1))}</strong>t ${reductietekst} </span>
               </div>
-                  <div class="caption_co2ok_widget co2ok_widget_info">
-                      <span> <strong>${(compensatiewidget.toFixed(1))}</strong>t CO₂ reductie </span>
-                      </div>
-                  </div>
-                      
-              <div class="co2ok_widget_infobox_container co2ok-popper" id="widget-infobox-view">
-
-              <div class="widget-inner-wrapper">
-              <a href="#!" input type="text" role="button" tabindex="0" class="selectable-text first-text-to-select" style="outline: none; -webkit-appearance: none;">
-              <p class="widget-text-block greyBorder">${compensatietekst} </p>
-              </a>
-              <img alt="Maak mijn aankoop klimaatneutraal " title="Maak mijn aankoop klimaatneutraal " src="${SITE_HOST}/widget/vliegtuig_hover.png" class="widget-svg-img-large  co2ok_info_hover_image">
-              </div>
-      
-              <a class="widget-hover-link" target="_blank" href="http://co2ok.eco"><img src="${SITE_HOST}/static/logo.png" class="co2ok_logo_default_info widget-hover-link co2ok_logo_default_info"></a>
-              <span class="widget-hover-link">
-              <a  class="widget-hover-link" target="_blank" href="http://www.co2ok.eco/co2-compensatie">Hoe werkt CO2 compensatie?</a> </span>
-              </div>
-      
-              <div class="co2ok_infobox_container co2ok-popper" id="infobox-view">    </div>
+          </div>
               
-              `
-              // console.log(widgetimg)
+      <div class="co2ok_widget_infobox_container co2ok-popper" id="widget-infobox-view">
 
-      // 1. Remove widgetContainer
-      document.getElementById("widgetContainer").remove()
-      // 2. Append widgetcontainer to targeted div
-      jQuery("<div id='widgetContainer' style='width:180px;height:auto;display:flex;flex-direction:row;justify-content:center;align-items:center;'></div>").appendTo(document.getElementsByClassName("col-md-pull-4")[0])
+      <div class="widget-inner-wrapper">
+      <a href="#!" input type="text" role="button" tabindex="0" class="selectable-text first-text-to-select" style="outline: none; -webkit-appearance: none;">
+      <p class="widget-text-block greyBorder">${compensatietekst} </p>
+      </a>
+      <img alt="${titletekst}" title="${titletekst}" src="${SITE_HOST}/widget/vliegtuig_hover.png" class="widget-svg-img-large  co2ok_info_hover_image">
+      </div>
+
+      <a class="widget-hover-link" target="_blank" href="http://co2ok.eco"><img src="${SITE_HOST}/static/logo.png" class="co2ok_logo_default_info widget-hover-link co2ok_logo_default_info"></a>
+      <span class="widget-hover-link">
+      <a  class="widget-hover-link" target="_blank" href="http://www.co2ok.eco/co2-compensatie">${how_does_it_worktekst}</a> </span>
+      </div>
+
+      <div class="co2ok_infobox_container co2ok-popper" id="infobox-view">    </div>
+      `
+              // console.log(widgetimg)
 
       let widgetcontainer = document.getElementById(widgetContainer)
       // let widgetcontainer = document.getElementsByClassName('whb-empty-column')[0]
+
+      // Don't try to place widget if there is no container
+      if(widgetcontainer == null){
+        return
+      }
+
       widgetcontainer.innerHTML = widgetmark
       this.RegisterWidgetInfoBox();
       // this.ShowWidgetInfoBox();
