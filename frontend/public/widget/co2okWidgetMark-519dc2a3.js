9e9429c59490bf549d823975a55a9cc6b804dc07
Jessica Smith <JessicaSmith@Jessies-MacBook-Pro.local>
2020-09-25 12:45:33 +0200

    revert SITE_HOST
    back to co2ok

--- frontend/public/widget/co2okWidgetMark-519dc2a3.js
+++ frontend/public/widget/co2okWidgetMark-519dc2a3.js
@@ -1,7 +1,7 @@
 let Co2okWidgetXL = {
 
-    // SITE_HOST: "https://co2ok.eco",
-    SITE_HOST: "http://localhost:8080",
+    SITE_HOST: "https://co2ok.eco",
+    // SITE_HOST: "http://localhost:8080",
 
     getCookieValue: function (a) {
       var b = document.cookie.match('(^|[^;]+)\\s*' + a + '\\s*=\\s*([^;]+)');
