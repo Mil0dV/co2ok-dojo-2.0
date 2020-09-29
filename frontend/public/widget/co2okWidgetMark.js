820f16b1b1457206904c2cbabaa69488cd3f552f
Milo de Vries <intensmilo@gmail.com>
2020-09-29 17:34:44 +0200

    Fixes double code

--- frontend/public/widget/co2okWidgetMark.js
+++ frontend/public/widget/co2okWidgetMark.js
@@ -306,15 +306,3 @@ if (document.currentScript.getAttribute('div')) {
   let lang = document.currentScript.getAttribute('lang')
   Co2okWidget.merchantCompensations(div, merchantId, widgetColor, lang)
 }
-
-// New style Async execution B)
-// if the variables are set on the script src, we're in async mode 
-// and don't expect the html to run merchantCompensations
-
-if (document.currentScript.getAttribute('div')) {
-  let div = document.currentScript.getAttribute('div')
-  let merchantId = document.currentScript.getAttribute('merchantId')
-  let widgetColor = document.currentScript.getAttribute('widgetColor')
-  let lang = document.currentScript.getAttribute('lang')
-  Co2okWidget.merchantCompensations(div, merchantId, widgetColor, lang)
-}
