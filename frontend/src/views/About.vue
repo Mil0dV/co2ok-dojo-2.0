73310d26d15252e88503cc582eed8669b1a4a444
Jessica Smith <jessicasmith@jessicadsmith.com>
2020-10-28 14:38:36 +0100

    updated team page
    removed team members and added myself :)

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -93,7 +93,6 @@
                                         <span class="icon__wrapper">
                                             <span uk-icon="linkedin"
                                                   class="crew__icon">
-
                                             </span>
                                         </span>
                                         LinkedIn
@@ -126,49 +125,25 @@
                         name: 'Milo de Vries', title: 'Founder',
                         quote: 'Making consumerism sustainable 💚🌍',
                         image: 'milo.jpg',
-                        twitter: '', facebook: ''
-                    },
-                    {
-                        name: 'Nick Maier', title: 'Co-Founder',
-                        quote: 'The climate is important and companies can have a big impact, either good or bad',
-                        image: 'nick.jpg',
-                        twitter: '', facebook: ''
+                        twitter: '', linkedin: ''
                     },
                     {
                         name: 'Lodi de Brauw', title: 'Co-Founder',
                         quote: 'You must be the change you want to see in the world',
                         image: 'lodi.jpg',
-                        twitter: '', facebook: ''
+                        twitter: '', linkedin: ''
                     },
                     {
                         name: 'Maaike Braat', title: 'Scientist',
                         quote: 'It can always be quicker and better, but this is a step in the right direction',
                         image: 'maaike.jpeg',
-                        twitter: '', facebook: ''
+                        twitter: '', linkedin: ''
                     },
                     {
-                        name: 'Michiel van Tienhoven', title: "Programmer and Graphic Designer",
-                        quote: 'More green is always better, after all we only have one planet earth',
-                        image: 'michiel-1.jpeg',
-                        twitter: '', facebook: ''
-                    },
-                    {
-                        name: 'Krista Peters', title: 'Trainee programming & Web Development ',
-                        quote: 'Let\'s try and speed up the transition to a greener world.',
-                        image: 'krista.jpg',
-                        twitter: '', facebook: ''
-                    },
-{
-                        name: 'Ahlam Saber', title: 'Partner Manager',
-                        quote: 'Silence is evil itself. Not to speak is to speak. Not to act is to act',
-                        image: 'ahlam.jpeg',
-                        twitter: '', facebook: ''
-                    },
-                    {
-                        name: 'Sanne Tielemans', title: 'Finance/Admin Assistant',
-                        quote: 'The greener, the better',
-                        image: 'sanne.jpeg',
-                        twitter: '', facebook: ''
+                        name: 'Jessica D. Smith', title: 'Software Engineer Trainee',
+                        quote: 'Sustainability now for a better future',
+                        image: 'jessica.jpg',
+                        twitter: '', linkedin: ''
                     },
 
                 ]
