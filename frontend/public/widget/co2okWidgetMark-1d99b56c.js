5afc8cb51ee333239c82f018fbbfe4c88588cac9
mvhou <mark.vanhoutert@gmail.com>
2021-03-17 22:53:25 +0100

    fixed and added infoboxes

--- frontend/public/widget/co2okWidgetMark-1d99b56c.js
+++ frontend/public/widget/co2okWidgetMark-1d99b56c.js
@@ -2,8 +2,8 @@
 
 let Co2okWidget = {
 
-	SITE_HOST: "https://co2ok.eco",
-	// SITE_HOST: "http://localhost:8080",
+	// SITE_HOST: "https://co2ok.eco",
+	SITE_HOST: "http://localhost:8080",
 
 	getCookieValue: function (a) {
 	  var b = document.cookie.match('(^|[^;]+)\\s*' + a + '\\s*=\\s*([^;]+)');
@@ -112,10 +112,14 @@ let Co2okWidget = {
 
 		//bottom footer
 		let cfsHtml = `
-			<img class="cfs_hover_target_footer" src="${this.SITE_HOST}/widget/cfs-gray.png" style="width: 100px; height: 48px; margin-left: 8px;">
+			<img class="cfs_hover_target_footer" src="${this.SITE_HOST}/widget/cfs-gray.png" style="width: 130px; height: 71px; margin-left: 20px; margin-top: -10px;">
 		`
 		jQuery(".footer-primary").after(cfsHtml)
 
+        let cfsHtmlCart = `
+            <br><img class="cfs_hover_target_cart" src="${this.SITE_HOST}/widget/cfs.png" style="width: 160px; height: 67px; margin-top: 20px;">
+        `
+        jQuery(".widget_shopping_cart .payment-icons").after(cfsHtmlCart)
 	},
 
 	/** inserts UPS's for VELA */
@@ -127,20 +131,22 @@ let Co2okWidget = {
 			let productUsp = `
 				<img class="co2ok-usp-product" src="${this.SITE_HOST}/widget/vela/VL_heart.png">
 			`
-			jQuery(".wc-stripe-clear").before(productUsp)
+			jQuery("img[src$='https://velaapparelstg.wpengine.com/wp-content/uploads/2021/02/110-1106271_guaranted-safe-checkout-safe-and-secure-checkout.png']").after(productUsp);
 
 			//product description
 			let productDesc = jQuery(".product-short-description")[0].childNodes
 			let productText = `
-				<br><i class="wishlist-icon icon-heart" style="color: #26B43D"></i> Climate friendly product
+            <br><u class="product-climate-friendly"><i class="wishlist-icon icon-heart" style="color: #26B43D"></i>Climate friendly product</u>
 			`
 			productDesc[1].innerHTML += productText
 		}
 
 		//added marquee text
 		let marquee = jQuery("#fs-progress");
-		let marqueeText = `, always climate friendly <i class="wishlist-icon icon-heart" style="color: #26B43D"></i>`
-		marquee[0].innerHTML += marqueeText
+        if (marquee[0].innerHTML == "You Get Free Shipping!") {
+		    let marqueeText = ` Always climate friendly <i class="wishlist-icon icon-heart" style="color: #26B43D"></i>`
+		    marquee[0].innerHTML += marqueeText
+        }
 	},
 
 	insertHovercardHTML: function () {
@@ -266,7 +272,7 @@ let Co2okWidget = {
       </p>
     `
 		let widgetmark = `
-			<div>
+			<div class="btn_shop_co2ok">
 
 				<div class="btn_co2ok_widget co2ok_widget_info widget-small" href="#">
 				<span class="btn_co2ok_widget co2ok_widget_info trustmark-border widget-small">SHOP<img class="logo_co2ok_widget widget-small" src="${this.SITE_HOST}/static/logo.png"></span>
@@ -336,8 +342,8 @@ let Co2okWidget = {
         offset.top = offset.top + (infoHoverBox.height() + elementBox.width() / 2) + 6;
       }
     } else if (element_id == '.widget-small') {
-			offset.left -= infoHoverBox.width() / 2;
-			offset.top -= infoHoverBox.height();
+            offset.left -= infoHoverBox.width() / 2;
+            offset.top -= infoHoverBox.height() - jQuery(window).scrollTop();
 		} else if (element_id == '.cfs_hover_target') {
 			offset.left -= infoHoverBox.width() / 2;
 			if (offset.left < 0) {
@@ -356,8 +362,14 @@ let Co2okWidget = {
 			offset.top -= infoHoverBox.height();
 		} else if (element_id == '.co2ok-usp-product') {
 			offset.left -= infoHoverBox.width() / 2 - elementBox.width() / 2;
-			offset.top -= infoHoverBox.height();
-		}
+			offset.top -= 300 - jQuery(window).scrollTop() / 2;
+		} else if (element_id == '.product-climate-friendly') {
+            offset.left -= infoHoverBox.width();
+            offset.top -= 200 - jQuery(window).scrollTop() / 2;
+        } else if (element_id == '.cfs_hover_target_cart') {
+            offset.left -= 100;
+            offset.top -= 500;
+        }
 		else
 			return ;
 
@@ -399,6 +411,10 @@ let Co2okWidget = {
 			return ('.co2ok-small')
 		else if (jQuery(e.target).hasClass("widget-small"))
 			return ('.widget-small')
+        else if (jQuery(e.target).hasClass("product-climate-friendly"))
+            return ('.product-climate-friendly')
+        else if (jQuery(e.target).hasClass("cfs_hover_target_cart"))
+            return ('.cfs_hover_target_cart')
     else if (jQuery(e.target).hasClass("widget-large"))
       return ('.widget-large')
 		else if (jQuery(e.target).hasClass("co2ok-usp-product"))
