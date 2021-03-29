a59866589590f83eef213fa220ce36845390bf86
mvhou <mark.vanhoutert@gmail.com>
2021-03-29 17:19:14 +0200

    reverted site_host

--- frontend/public/widget/co2okWidgetMark-2786c947.js
+++ frontend/public/widget/co2okWidgetMark-2786c947.js
@@ -1,9 +1,9 @@
 //VELA
 
-Co2okWidget = {
+let Co2okWidget = {
 
 	// SITE_HOST: "https://co2ok.eco",
-	SITE_HOST: "http://localhost:8080",
+	// SITE_HOST: "http://localhost:8080",
 
 	getCookieValue: function (a) {
 	  var b = document.cookie.match('(^|[^;]+)\\s*' + a + '\\s*=\\s*([^;]+)');
